6ee8355e543339409249336efec81ca3a096041f
Lauro Fialho Müller <contact@lauromueller.com>
2025-07-03 14:58:12 +0200

    feat(typing): add common_types files

---
+++ typing/common_types.py
@@ -0,0 +1,14 @@
+# Section: Typing Lists
+
+# Section: Typing Dictionaries
+
+# Section: Typing Tuples
+
+# Section: Typing Sets
+
+# Section: Optional[X] for Values That Can Be None
+
+# Section: Union[X, Y, ...] for Multiple Possible Types
+
+# Section: Any for Unrestricted Types
+
