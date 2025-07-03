7c77ab3c21a353cea8e3e43d27f1ad1aed697680
Lauro Fialho Müller <contact@lauromueller.com>
2025-07-03 14:58:29 +0200

    feat(multi-file-projects): add subpackages file - complete code

--- multi-file-projects/main.py
+++ multi-file-projects/main.py
@@ -1,6 +1,10 @@
 print("Main script starting...")
 
-from devops_utils import check_file_extension
+from devops_utils import (
+    check_file_extension,
+    is_host_up,
+    check_hosts_from_config,
+)
 import sys
 
 print(sys.path)
@@ -10,3 +14,12 @@ filenames = ["config.yaml", "script.sh"]
 for filename in filenames:
     print(f"Checking {filename}")
     print(f"Result: {check_file_extension(filename)}")
+
+print(f"\nIs localhost up? {is_host_up("localhost")}")
+print(
+    f"Is nonexistenthost12345 up? {is_host_up("nonexistenthost12345")}"
+)
+
+print(
+    f"\nAre all hosts from servers_config.yaml up? {check_hosts_from_config("servers_config.yaml")}"
+)
