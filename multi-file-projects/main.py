cbed7c54539ed9b2f891060a18c48d8de6a5b82f
Lauro Fialho Müller <contact@lauromueller.com>
2025-07-03 14:58:28 +0200

    feat(multi-file-projects): add python_packages file - complete code

--- multi-file-projects/main.py
+++ multi-file-projects/main.py
@@ -1,6 +1,6 @@
 print("Main script starting...")
 
-from file_ops import check_file_extension as check_file
+from devops_utils import check_file_extension
 import sys
 
 print(sys.path)
@@ -9,4 +9,4 @@ filenames = ["config.yaml", "script.sh"]
 
 for filename in filenames:
     print(f"Checking {filename}")
-    print(f"Result: {check_file(filename)}")
+    print(f"Result: {check_file_extension(filename)}")
